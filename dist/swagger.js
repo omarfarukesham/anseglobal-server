f3b06e94d86ff7f50b07eddd2edbe951f667460e
omarfarukesham <omar.lu86@gmail.com>
2025-02-05 14:00:15 +0600

    feat: swagger doc api url updated

--- dist/swagger.js
+++ dist/swagger.js
@@ -14,7 +14,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:5000',
+                url: 'https://anseglobal-server.vercel.app',
                 description: 'Local server',
             },
         ],
